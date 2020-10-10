f7954db53c6ad0eed23fd967bb3a3aa112a5249f
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2020-10-10 18:56:54 +0300

    build: completed versioning

--- scripts/set-package-version.js
+++ scripts/set-package-version.js
@@ -3,16 +3,14 @@ const { exec } = require("child_process");
 let { PACKAGE_VERSION } = process.env;
 const [,, buildVersion] = process.argv;
 
-// if (!PACKAGE_VERSION || !buildVersion) {
-//   throw 'Version is not defined';
-// }
+if (!PACKAGE_VERSION || !buildVersion) {
+  throw 'Version is not defined';
+}
 
-PACKAGE_VERSION = '3.4.5';
+// PACKAGE_VERSION = '3.4.5';
 
 const [major, minor, patch] = PACKAGE_VERSION.split('.');
 
 const version = `${+/\d+/.exec(buildVersion)[0] * 10 + +major}.${minor}.${patch}`;
 
 exec(`cd ./projects/ng-utils && npm version ${version} --no-git-tag-version`);
-
-console.log(`thalesrc-ng-utils-${version}.tgz`);
