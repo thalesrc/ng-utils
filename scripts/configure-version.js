f71eb249e46764b2e0b0447c478509f1937992d0
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2020-10-10 14:11:54 +0300

    build: started testing version builds

--- scripts/configure-version.js
+++ scripts/configure-version.js
@@ -1,3 +1,43 @@
+const { readFileSync, writeFileSync } = require('fs');
+const { resolve } = require('path');
 const [,, version] = process.argv;
 
-console.log(args);
+// ----------------------------------------
+
+const initialPackage = JSON.parse(readFileSync(resolve(__dirname, '../package.json'), 'utf8'));
+const versionPackage = JSON.parse(readFileSync(resolve(__dirname, `../package.${version}.json`), 'utf8'));
+
+const modifiedPackage = {
+  ...initialPackage,
+  ...versionPackage,
+  dependencies: {
+    ...initialPackage.dependencies,
+    ...versionPackage.dependencies
+  },
+  devDependencies: {
+    ...initialPackage.devDependencies,
+    ...versionPackage.devDependencies
+  }
+};
+
+writeFileSync(resolve(__dirname, '../package.json'), JSON.stringify(modifiedPackage), 'utf8');
+
+// ----------------------------------------
+
+const initialLibraryPackage = JSON.parse(readFileSync(resolve(__dirname, '../projects/ng-utils/package.json'), 'utf8'));
+const versionLibraryPackage = JSON.parse(readFileSync(resolve(__dirname, `../projects/ng-utils/package.${version}.json`), 'utf8'));
+
+const modifiedVersionPackage = {
+  ...initialLibraryPackage,
+  ...versionLibraryPackage,
+  dependencies: {
+    ...initialLibraryPackage.dependencies,
+    ...versionLibraryPackage.dependencies
+  },
+  peerDependencies: {
+    ...initialLibraryPackage.peerDependencies,
+    ...versionLibraryPackage.peerDependencies
+  }
+};
+
+writeFileSync(resolve(__dirname, '../projects/ng-utils/package.json'), JSON.stringify(modifiedVersionPackage), 'utf8');
