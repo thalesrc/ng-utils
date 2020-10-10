d800c9f9b895cef242bcc28b0f9f75fdfa56e42f
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2020-10-10 16:21:45 +0300

    build: env test

--- scripts/set-package-version.js
+++ scripts/set-package-version.js
@@ -17,4 +17,5 @@ const version = `${+/\d+/.exec(buildVersion)[0] * 10 + +major}.${minor}.${patch}
 
 exec(`cd ./projects/ng-utils && npm version ${version} --no-git-tag-version`);
 
-console.log(`thalesrc-ng-utils-${version}.tgz`);
+// console.log(`thalesrc-ng-utils-${version}.tgz`);
+console.log(JSON.stringify(process.env) + ':::' + JSON.stringify(process.argv));
