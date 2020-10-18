6c2380180464f7dd664f0d36d82aba9d5906aefc
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2020-10-18 21:11:55 +0300

    fix: barrel fix

--- projects/ng-utils/main.entry.ts
+++ projects/ng-utils/main.entry.ts
@@ -1,7 +1,8 @@
-export * from '@thalesrc/ng-utils/advanced-route';
-export * from '@thalesrc/ng-utils/form';
-export * from '@thalesrc/ng-utils/overlay';
-export * from '@thalesrc/ng-utils/resize';
-export * from '@thalesrc/ng-utils/substitute';
+// tslint:disable:max-line-length
 
+export { AdvancedRouteService } from '@thalesrc/ng-utils/advanced-route';
+export { ArrayModule, FormArrayDirective, FormArrayItemDirective, FormDisabledDirective, FormModule, ImageInputDirective, ImageInputModule } from '@thalesrc/ng-utils/form';
+export { OverlayComponent, OverlayConfig, OverlayModule } from '@thalesrc/ng-utils/overlay';
+export { AnimateResizeDirective, ResizeDirective, ResizeModule, ResizeService } from '@thalesrc/ng-utils/resize';
+export { SubstituteComponent, SubstituteDirective, SubstituteModule } from '@thalesrc/ng-utils/substitute';
 export {InputStream, ListenerStream, Unsubscriber} from '@thalesrc/ng-utils/utils';
